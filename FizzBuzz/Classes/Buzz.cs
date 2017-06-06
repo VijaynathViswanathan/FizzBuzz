7172266166a6ce6e684a65cdd08c0e79c7fb08d3
Vijayanath Viswanathan <vijaynathv@gmail.com>
2017-06-06 20:59:48 +0100

    First Commit

---
+++ FizzBuzz/Classes/Buzz.cs
@@ -0,0 +1,15 @@
+﻿using FizzBuzz.Interface;
+using System;
+
+namespace FizzBuzz.Classes
+{
+    public class Buzz : IFizzBuzzHandler
+    {
+        public string Handle(int number)
+        {
+            if (number % 5 == 0)
+                return "Buzz";
+            return number.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using FizzBuzz.Interface;
+using FizzBuzz.Classes;
+using System;
+
+namespace FizzBuzz.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFizzBuzzHandler fizzBuzzHandler = new Classes.FizzBuzz(new Fizz(new Buzz()));
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(fizzBuzzHandler.Handle(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
